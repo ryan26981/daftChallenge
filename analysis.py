b210a873bf4490b6eb1b9b4188465d9abed5c799
Ryan Smolowyk <ryansm@udel.edu>
2026-03-09 19:39:26 -0400

    Completed the first two images

---
+++ analysis.py
@@ -0,0 +1,13 @@
+# Install DAFT package (run this in terminal if not already installed)
+# pip install 'daft-pgm'
+
+import daft
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Set up plotting parameters for better quality
+plt.rcParams['figure.dpi'] = 150
+plt.rcParams['savefig.dpi'] = 300
+
+print("DAFT package imported successfully!")
+print("Ready to create beautiful DAGs!")
